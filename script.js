0e7822fc904531431e2671527f0663141a4f0100
magdyZiad <magdyziad.dev@gmail.com>
2026-07-20 14:58:13 +0300

    add new section

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let menuBtn = document.getElementById("menu")
 let mobileNav = document.getElementById("mobileNav")
+let mainNavBar = document.getElementById("nav")
 
-menuBtn.addEventListener("click", ()=>{
+menuBtn.addEventListener("click", () => {
     if (mobileNav.classList.contains("hidden")) {
         mobileNav.classList.remove("hidden")
         mobileNav.classList.add("flex")
@@ -14,10 +15,20 @@ menuBtn.addEventListener("click", ()=>{
 })
 
 let links = Array.from(document.querySelectorAll("#mobileNav li a"))
-links.forEach( e => {
-    e.addEventListener("click", ()=>{
+links.forEach(e => {
+    e.addEventListener("click", () => {
         mobileNav.classList.remove("flex")
         mobileNav.classList.add("hidden")
         menuBtn.innerHTML = `<i class="fa-solid fa-bars"></i>`
     })
 })
+
+window.onscroll = () => {
+    if (scrollY > 70) {
+        mainNavBar.classList.remove("bg-white")
+        mainNavBar.classList.add("bg-white/10", "backdrop-blur-lg")
+    }else {
+        mainNavBar.classList.remove("bg-white/10", "backdrop-blur-lg")
+        mainNavBar.classList.add("bg-white")
+    }
+}
