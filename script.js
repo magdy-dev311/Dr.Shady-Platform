3dda2f92177acb9290180e0977b17a48ca3cf8b6
magdyZiad <magdyziad.dev@gmail.com>
2026-07-20 15:10:24 +0300

    add scroll to Top Btn

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let menuBtn = document.getElementById("menu")
 let mobileNav = document.getElementById("mobileNav")
 let mainNavBar = document.getElementById("nav")
+let scrollToTopBtn = document.getElementById("scrollToTop")
 
 menuBtn.addEventListener("click", () => {
     if (mobileNav.classList.contains("hidden")) {
@@ -27,8 +28,16 @@ window.onscroll = () => {
     if (scrollY > 70) {
         mainNavBar.classList.remove("bg-white")
         mainNavBar.classList.add("bg-white/10", "backdrop-blur-lg")
+        scrollToTopBtn.classList.remove("translate-x-[100px]")
+        scrollToTopBtn.classList.add("translate-x-0")
     }else {
         mainNavBar.classList.remove("bg-white/10", "backdrop-blur-lg")
         mainNavBar.classList.add("bg-white")
+        scrollToTopBtn.classList.remove("translate-x-0")
+        scrollToTopBtn.classList.add("translate-x-[100px]")
     }
 }
+
+scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo(0,0)
+})
